403b7324d4af60dec8faeb6baa61ade2fd026c63
mogonchi <monyenyedolvine@gmail.com>
2022-03-22 19:11:46 +0300

    add css rule to id girla and class male clothes

--- js/children.js
+++ js/children.js
@@ -1,19 +1,24 @@
 
-function Clothe(name, price){
-    this.name = name;
+// function Clothe(name, price){
+//     this.name = name;
     
-    this.price = price;
-};
+//     this.price = price;
+// };
 
-$(document).ready(function(){
-    $("a#buy").last().click(function(event){
-        event.preventDefault();
+// $(document).ready(function(){
+//     $("a#buy").last().click(function(event){
+//         event.preventDefault();
 
-        var itemName = $("h5#name").text();
-        var itemPrice = parseInt($("p#price").text());
+//         var itemName = $("h5#name").text();
+//         var itemPrice = parseInt($("p#price").text());
         
-        var selectedClothe = new Clothe (itemName, itemPrice);
+//         var selectedClothe = new Clothe (itemName, itemPrice);
 
-        console.log(selectedClothe);
-    });
-})
+//         console.log(selectedClothe);
+//     });
+// })
+
+// switch(new Clothe){
+//     case 0:
+//         dress =100
+// }
