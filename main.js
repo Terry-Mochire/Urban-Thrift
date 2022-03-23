950d484acf058efac0fe6a03ce7970019cc65377
Terry <terrybmochire@gmail.com>
2022-03-23 04:25:26 +0300

    Write script that stores clicked item.

--- main.js
+++ main.js
@@ -1,39 +1,71 @@
 $(document).ready(function () {
     let carts = document.querySelectorAll(".add-cart");
 
-// function clothingItem(name, price, inCart) {
-//     this.name = name;
-//     this.price = price;
-//     this.inCart = 0
-// };
-
-// let products = [clothingItem()]
-
-for (let index = 0; index < carts.length; index++) {
-    carts[index].addEventListener('click', function(event) {
-        event.preventDefault();
-        cartNumbers();
-    });
     
-};
-function cartNumbers() {
-    let productNumbers = parseInt(localStorage.getItem('cartnumbers'));
-     if (productNumbers) {
+
+    for (let index = 0; index < carts.length; index++) {
+        carts[index].addEventListener('click', function(event) {
+            event.preventDefault();
+            console.log("Added to Cart");
+        
+            
+
+            const activeParent = document.activeElement.parentNode;
+           
+            console.log(activeParent);
+           
+                
+            function clothingItem(name, price, inCart) {
+                this.name = name;
+                this.price = price;
+                this.inCart = 0;
+            };
+            let products = [];
+            
+            let itemName = activeParent.querySelector("h5").textContent;
+            let itemPrice =activeParent.querySelector("#price").textContent;
+    
+            let selectedClothingItem = new clothingItem(itemName, itemPrice);
+            products.push(selectedClothingItem);
+        
+            console.log(products);
+
+            cartNumbers(products[index]);
+
+            
+
+        });
+
+    };
+
+    function cartNumbers(products) {
+
+        let productNumbers = parseInt(localStorage.getItem('cartnumbers'));
+        if (productNumbers) {
         localStorage.setItem('cartnumbers', productNumbers += 1);
         document.querySelector('.d-flex span').textContent = productNumbers + 1;
-     } else {
+        } else {
         localStorage.setItem('cartnumbers', 1);
         document.querySelector('.d-flex span').textContent = 1;
+        }
+
+        setItems(products);
+    };
+     function setItems(products) {
+         console.log("Inside of the SetItems Function");
+         console.log("My product is", products);
      }
-};
 
-function onLoadCartNumbers() {
-    let productNumbers = parseInt(localStorage.getItem('cartnumbers'));
+    function onLoadCartNumbers() {
+        let productNumbers = parseInt(localStorage.getItem('cartnumbers'));
 
-    if(productNumbers) {
+        if(productNumbers) {
         document.querySelector('.d-flex span').textContent = productNumbers;
-    }
+    };
 };
+    
+
+
 
 onLoadCartNumbers()
 });
